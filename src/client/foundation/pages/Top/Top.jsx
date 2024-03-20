766b31f06d7a9e1d1b540809c8429411819c1772
yu-3in <misharv.y@gmail.com>
2024-03-20 15:01:47 +0900

    fix: ヒーロイメージのAPIフェッチをやめる

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -85,26 +85,6 @@ function useTodayRacesWithAnimation(races) {
   return racesToShow;
 }
 
-/**
- * @param {Model.Race[]} todayRaces
- * @returns {string | null}
- */
-function useHeroImage(todayRaces) {
-  const firstRaceId = todayRaces[0]?.id;
-  const url =
-    firstRaceId !== undefined
-      ? `/api/hero?firstRaceId=${firstRaceId}`
-      : "/api/hero";
-  const { data } = useFetch(url, jsonFetcher);
-
-  if (firstRaceId === undefined || data === null) {
-    return null;
-  }
-
-  const imageUrl = `${data.url}?${data.hash}`;
-  return imageUrl;
-}
-
 /** @type {React.VFC} */
 export const Top = () => {
   const { date = dayjs().format("YYYY-MM-DD") } = useParams();
@@ -153,7 +133,7 @@ export const Top = () => {
           )
       : [];
   const todayRacesToShow = useTodayRacesWithAnimation(todayRaces);
-  const heroImageUrl = useHeroImage(todayRaces);
+  const heroImageUrl = "/assets/images/hero.webp";
 
   return (
     <Container>
