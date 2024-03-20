070416afc7fc3f85831aad62d198426e59214a24
yu-3in <misharv.y@gmail.com>
2024-03-20 17:56:26 +0900

    useAnimation削除

--- src/client/foundation/pages/Top/internal/RecentRaceList/RecentRaceList.jsx
+++ src/client/foundation/pages/Top/internal/RecentRaceList/RecentRaceList.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import { LinkButton } from "../../../../components/buttons/LinkButton";
-import { Spacer } from "../../../../components/layouts/Spacer";
 import { Stack } from "../../../../components/layouts/Stack";
-import { easeOutCubic, useAnimation } from "../../../../hooks/useAnimation";
 import { Color, FontSize, Radius, Space } from "../../../../styles/variables";
 import { formatCloseAt } from "../../../../utils/DateUtils";
 
@@ -16,11 +14,22 @@ export const RecentRaceList = ({ children }) => {
   );
 };
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
 const ItemWrapper = styled.li`
   background: ${Color.mono[0]};
   border-radius: ${Radius.MEDIUM};
   opacity: ${({ $opacity }) => $opacity};
   padding: ${Space * 3}px;
+
+  animation: ${fadeIn} 0.5s cubic-bezier(0.2, 0.6, 0.35, 1);
 `;
 
 const RaceButton = styled(LinkButton)`
@@ -45,7 +54,7 @@ const RaceTitle = styled.h2`
  */
 
 /** @type {React.VFC<ItemProps>} */
-const Item = ({ race }) => {
+const Item = React.memo(({ race }) => {
   const [closeAtText, setCloseAtText] = useState(formatCloseAt(race.closeAt));
 
   // 締切はリアルタイムで表示したい
@@ -59,45 +68,28 @@ const Item = ({ race }) => {
     };
   }, [race.closeAt]);
 
-  const {
-    abortAnimation,
-    resetAnimation,
-    startAnimation,
-    value: opacity,
-  } = useAnimation({
-    duration: 500,
-    end: 1,
-    start: 0,
-    timingFunction: easeOutCubic,
-  });
-
-  useEffect(() => {
-    resetAnimation();
-    startAnimation();
-
-    return () => {
-      abortAnimation();
-    };
-  }, [race.id, startAnimation, abortAnimation, resetAnimation]);
-
   return (
-    <ItemWrapper $opacity={opacity}>
-      <Stack horizontal alignItems="center" justifyContent="space-between">
+    <ItemWrapper>
+      <Stack
+        horizontal
+        alignItems="center"
+        gap={Space * 2}
+        justifyContent="space-between"
+      >
         <Stack gap={Space * 1}>
           <RaceTitle>{race.name}</RaceTitle>
           <p>{closeAtText}</p>
         </Stack>
 
-        <Spacer mr={Space * 2} />
-
         <Stack.Item grow={0} shrink={0}>
           <Stack horizontal alignItems="center" gap={Space * 2}>
             <img
-              height={100}
-              width={100}
               alt=""
+              height={100}
+              loading="lazy"
               src={race.image.replace(".jpg", ".webp")}
               style={{ objectFit: "cover" }}
+              width={100}
             />
             <RaceButton to={`/races/${race.id}/race-card`}>投票</RaceButton>
           </Stack>
@@ -105,5 +97,8 @@ const Item = ({ race }) => {
       </Stack>
     </ItemWrapper>
   );
-};
+});
+
+Item.displayName = "RecentRaceListItem";
+
 RecentRaceList.Item = Item;
